97ff6dc3a5366e668d71286de6b926c87698ef61
salimssa <salimssaly@outlook.com>
2021-01-23 17:30:03 -0500

    polishing player movement and adding in probuilder

---
+++ LDTest FTE SalimAli/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed = 10;
+
+    private float moveInput = 0;
+    public float horizontalDampingBasic;
+    public float horizontalDampingWhenStopping;
+    public float horizontalDampingWhenTurning;
+
+    public float jumpVelocity = 10.0f;
+
+
+    public float fallMultiplier = 3f;
+    public float lowJumpMultiplier = 6f;
+    public float jumpHeldMultiplier = 2f;
+    public float hangTime = 0.2f;
+    private float hangCounter;
+    public float jumpBufferLength = 0.1f;
+    private float jumpBufferCount;
+    public bool applyJumpHeldMult { get; set; } = false;
+
+
+    private bool isGrounded = false;
+    public float groundCheckDist;
+    public Transform feetPos;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void GroundCheck()
+    {
+        if (Physics.Raycast(feetPos.position, Vector3.down, groundCheckDist))
+        {
+            isGrounded = true;
+        }
+    }
+
+
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        float horizontalVelocity = rb.velocity.x;
+        horizontalVelocity += moveInput;
+
+        // speed ramps down to zero when stopping
+        if (Mathf.Abs(moveInput) < 0.01f)
+        { horizontalVelocity *= Mathf.Pow(1f - horizontalDampingWhenStopping, Time.deltaTime * 10f); }
+        // Smooth turns
+        else if (Mathf.Sign(moveInput) != Mathf.Sign(horizontalVelocity))
+        { horizontalVelocity *= Mathf.Pow(1f - horizontalDampingWhenTurning, Time.deltaTime * 10f); }
+        // smoothens speed ramp up to max from 0
+        else
+        { horizontalVelocity *= Mathf.Pow(1f - horizontalDampingBasic, Time.deltaTime * 10f); }
+
+        rb.velocity = new Vector3(horizontalVelocity, rb.velocity.y, rb.velocity.z);
+
+
+
+
+        GroundCheck();
+
+        if (!isGrounded)
+        {
+            if (rb.velocity.y < 0)
+            { rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime; }
+            else if (rb.velocity.y > 0 && Input.GetButton("Jump") && applyJumpHeldMult)
+            { rb.velocity += Vector3.up * Physics.gravity.y * (jumpHeldMultiplier - 1) * Time.deltaTime; }
+            else
+            { rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime; }
+        }
+
+        // Jump
+        if (hangCounter > 0 && jumpBufferCount > 0 && isGrounded && rb.velocity.y == 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+            hangCounter = 0;
+            jumpBufferCount = 0;
+            isGrounded = false;
+            applyJumpHeldMult = true;
+        }
+
+    }
+
+    void Update()
+    {
+        // Jump hang time
+        if (isGrounded) { hangCounter = hangTime; }
+        else { hangCounter -= Time.deltaTime; }
+
+        // jump buffer
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCount = jumpBufferLength;
+        }
+        else
+        {
+            jumpBufferCount -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonUp("Jump") && applyJumpHeldMult)
+        {
+            applyJumpHeldMult = false;
+        }
+
+    }
+
+}
